04c2ed9e8e9c8d22216c0038d449a7c7913284c5
Simone Stefani <s.stefani95@gmail.com>
2017-11-05 11:14:41 -0800

    Add writer methods in vga_buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,33 @@
 // File attributes
 #![feature(lang_items)]
+#![feature(const_fn)] // Static initializers
+#![feature(unique)]
+#![feature(const_unique_new)]
 #![no_std] // Don't link std lib
 
 extern crate rlibc;
+
+#[macro_use]
+mod vga_buffer;
 
 // Disable name mangling to following fn
 // since we want to call it from assembly
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Hello World! Pew!";
-    let color_byte = 0x1f; // white foreground, blue background
+    // let hello = b"Hello World! Pew!";
+    // let color_byte = 0x1f; // white foreground, blue background
 
-    let mut hello_colored = [color_byte; 34];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
+    // let mut hello_colored = [color_byte; 34];
+    // for (i, char_byte) in hello.into_iter().enumerate() {
+    //     hello_colored[i*2] = *char_byte;
+    // }
 
-    // write `Hello World!` to the center of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
+    // // write `Hello World!` to the center of the VGA text buffer
+    // let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    // unsafe { *buffer_ptr = hello_colored };
 
-    loop{}
+    // loop{}
+    vga_buffer::print_something();
 }
 
 // Handle unwinding and panic
