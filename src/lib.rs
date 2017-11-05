d45e07ad4e088f760874029fecf716430a07726d
Simone Stefani <s.stefani95@gmail.com>
2017-11-05 10:48:53 -0800

    Print centered hello world

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,19 @@ extern crate rlibc;
 // since we want to call it from assembly
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", "World", "!"];
-    let y = x;
+    let hello = b"Hello World! Pew!";
+    let color_byte = 0x1f; // white foreground, blue background
+
+    let mut hello_colored = [color_byte; 34];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // write `Hello World!` to the center of the VGA text buffer
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop{}
 }
 
 // Handle unwinding and panic
