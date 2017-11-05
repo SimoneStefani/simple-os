fe9ade7a82d492a8b6420c05064ec56ae216b280
Simone Stefani <s.stefani95@gmail.com>
2017-11-05 07:32:31 -0800

    Implement stack

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,6 +3,19 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_top
     ; print 'OK' to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+error:
+    mov dword [0xb8000], 0x4f524f45 ; 0x52=R 0x45=E
+    mov dword [0xb8004], 0x4f3a4f52 ; 0x3a=colon
+    mov dword [0xb8008], 0x4f204f20 ; 0x20=whitespace
+    mov dword [0xb800a], al
+    hlt
+
+section .bss
+stack_bottom:
+    resb 64
+stack_top:
