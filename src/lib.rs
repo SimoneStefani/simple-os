4fcb4f4795e9f3b8c881f522946886efbd12d2c3
Simone Stefani <s.stefani95@gmail.com>
2017-11-15 07:46:32 -0800

    Print more info on memory

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,11 @@ mod vga_buffer;
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
     
+    // Say hello! :-)
     vga_buffer::clear_screen();
     println!("Hello World{}", "!");
 
+    // Print memory mapping
     let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
     let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
 
@@ -28,6 +30,21 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     for area in memory_map_tag.memory_areas() {
         println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
     }
+
+    let elf_sections_tag = boot_info.elf_sections_tag().expect("Elf-sections tag required");
+
+    println!("kernel sections:");
+    for section in elf_sections_tag.sections() {
+        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section.addr, section.size, section.flags);
+    }
+
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
+    println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+    println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
 
     loop{}
     
